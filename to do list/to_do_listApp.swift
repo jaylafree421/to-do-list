a6a00f3a2de1ba96cbd0c4e575bb730014739cd1
Jayla F <jfreeman21354@gmail.com>
2023-06-13 11:27:25 -0500

    Initial Commit

---
+++ to do list/to_do_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  to_do_listApp.swift
+//  to do list
+//
+//  Created by Scholar on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct to_do_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
